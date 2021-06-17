a4cf1c7b15db3f827b5f1a10f171b8c7ac3e4af8
WolfenCLI <WolfenCLI@zohomail.eu>
2021-06-17 22:31:01 +0200

    settings change

--- sudoku/main.py
+++ sudoku/main.py
@@ -2,7 +2,7 @@ import os
 
 from discord.ext import commands
 
-from sudoku.prepare import get_settings, cogs_list, add_cogs_to_bot
+from sudoku.prepare import get_settings, cogs_list, add_cogs_to_bot, env_to_settings
 from sudoku.servers.config import server_prefix
 
 settings_file = "./settings.toml"
@@ -12,6 +12,7 @@ token = os.getenv("BOT_TOKEN")
 bot: commands.bot = commands.Bot(command_prefix=server_prefix)
 
 settings: dict = get_settings(settings_file)
+env_to_settings(settings)
 cogs: list = cogs_list(settings['paths']['cogs'])
 
 add_cogs_to_bot(bot, settings, cogs)
