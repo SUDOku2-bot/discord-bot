bbe2c2676807d38847bc14f8f6aaa85aaaaa1f26
WolfenCLI <WolfenCLI@zohomail.eu>
2021-07-28 20:45:25 +0200

    implements roles on level up TODO: implement remove role

--- sudoku/cogs/leveling.py
+++ sudoku/cogs/leveling.py
@@ -1,7 +1,10 @@
 import json
 
 import discord
+import requests
+from discord.utils import get
 from discord.ext import commands
+from discord.ext.commands import has_permissions
 
 from sudoku.api_utils import patch_request, error_chk
 
@@ -13,6 +16,9 @@ class Leveling(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
+        if message.author.bot:
+            return
+
         api_url = self.settings['API_URL'] + "levels/xp"
         response = patch_request(api_url, data={
             "server_id": str(message.guild.id),
@@ -31,12 +37,38 @@ class Leveling(commands.Cog):
         if not res['trigger']:
             return
 
+        # Level up happened
+        await self.on_level_up(message, res['level'])
+
         await message.channel.send(f"<@{message.author.id}> is now level {res['level']}")
-        # TODO: Check if user needs to get a role
+
+    async def on_level_up(self, message: discord.Message, level: int):
+        response = requests.get(f"{self.settings['API_URL']}levels/role", params={
+            "server_id": message.guild.id,
+            "level": level
+        })
+
+        json_body = response.json()
+        if json_body is None or json_body['status'] != "ok":
+            return
+
+        json_body['content']: list
+        if len(json_body['content']) < 1:
+            return
+
+        author: discord.Member = message.author
+        for role_id in json_body['content']:
+            try:
+                role = get(message.guild.roles, id=int(role_id))
+                await author.add_roles(role, reason="Level up")
+            except discord.errors.Forbidden as e:
+                await message.channel.send(f"Error: {e}")
+                return
 
     @commands.command(aliases=['lvltoggle', 'levelstoggle', 'leveling'])
+    @has_permissions(administrator=True)
     async def toggle_leveling(self, ctx: commands.Context):
-        # TODO: Check user's permissions lmao
+        print("AAAAAAAAAAAAAAAAA")
         api_url = self.settings['API_URL']
         response = patch_request(f"{api_url}servers", data={
             "leveling": 1,
@@ -49,6 +81,35 @@ class Leveling(commands.Cog):
             await ctx.send(f"Error!\n```\n{res['content']}\n```")
 
         await ctx.send(f"Leveling {'enabled' if res['content'] else 'disabled'}")
+
+    @commands.command(aliases=['roleup', 'levels_role', 'levelrole', 'levelsrole'])
+    @has_permissions(ban_members=True)
+    async def level_role(self, ctx: commands.Context, mode: str, role: discord.Role = None, level: int = None):
+        if mode == "add":
+            if level is None or role is None:
+                await ctx.send("Usage: <add/remove> <role_id> <level>")
+                return
+
+            body = {
+                "server_id": str(ctx.guild.id),
+                "role_id": str(role.id),
+                "level": level
+            }
+
+            headers = {
+                "content-type": "application/json"
+            }
+
+            response = requests.post(f"{self.settings['API_URL']}levels/role", data=json.dumps(body), headers=headers)
+            res_json = response.json()
+
+            if "status" not in res_json.keys() or res_json["status"] != "ok":
+                await ctx.send(f"Error while executing the command: {res_json['error']}")
+                return
+
+            await ctx.send(f"Set Automatic Role setup for role: `{role}` at level `{level}`")
+        elif mode == "remove":
+            pass
 
 
 def init_class(bot: commands.Bot, settings: dict):
