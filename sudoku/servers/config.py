2084a61521bedeab35c62dce4a20dc84d5bfb4bf
WolfenCLI <WolfenCLI@zohomail.eu>
2021-08-02 13:34:07 +0200

    LRU cache for prefix

--- sudoku/servers/config.py
+++ sudoku/servers/config.py
@@ -1,5 +1,6 @@
 import json
 import os
+from functools import lru_cache
 
 import discord.ext.commands
 import requests
@@ -11,10 +12,19 @@ prefix_url = f"{API_url}servers/prefix"
 
 
 def server_prefix(bot: discord.ext.commands.Bot, message: discord.Message):
-    response = requests.get(prefix_url, params={"id": message.guild.id})
+    prefix: str = get_prefix(message.guild.id)
+    if prefix is None:
+        logger.error(f"server_prefix | [{message.author.id}] : [{message.content}]")
+        return "su!"
+    return prefix
+
+
+@lru_cache(maxsize=128)
+def get_prefix(server_id: int):
+    response = requests.get(prefix_url, params={"id": server_id})
     if response.status_code != 200:
         res: dict = json.loads(response.text)
-        logger.error(f"server_prefix | [{message.author.id}] : [{message.content}]")
         logger.error(f"{res}")
+        return None
 
     return response.text
